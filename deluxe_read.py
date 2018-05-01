d207037e42eb54d31907edef64bdea6b97548cbd
Yuriy <36294649+Shtokhman@users.noreply.github.com>
2018-05-01 14:08:36 +0300

    Add files

---
+++ deluxe_read.py
@@ -0,0 +1,28 @@
+import json
+from urllib.request import urlopen
+
+# Reading URL
+data = json.loads(urlopen("http://95.46.45.206:8000/public/goods").read())
+
+# Writing info from URl into json file
+with open("DeluxeMenu_info.json", "w") as file:
+    json.dump(data, file, indent=4)
+menu = {}
+
+# Adding categories
+for position in data:
+    menu[position['subCategoryResponse']['categoryResponse']['name']]\
+        = dict()
+# Adding subcategories as values of appropriate categories
+for position in data:
+    category = position['subCategoryResponse']['categoryResponse']['name']
+    menu[category].update({position['subCategoryResponse']['name']: []})
+
+# Adding dishes to an appropriate subcategory
+for position in data:
+    category = position['subCategoryResponse']['categoryResponse']['name']
+    subcategory = position['subCategoryResponse']['name']
+    menu[category][subcategory].append(position['name'])
+
+
+print(menu)
